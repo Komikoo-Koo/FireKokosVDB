425d68d28dc2f5c7e4d691efaeb974694d2ae80d
Komikoo-Koo <Komikoocontact@gmail.com>
2025-02-28 13:14:27 -0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+window.firebaseConfig = {
+            apiKey: "AIzaSyAXnxjDWDIa6fVliZOOWAZ6-9Tjapppl8c",
+            authDomain: "wholesome-de54b.firebaseapp.com",
+            databaseURL: "https://wholesome-de54b-default-rtdb.firebaseio.com",
+            projectId: "wholesome-de54b",
+            storageBucket: "wholesome-de54b.appspot.com",
+            messagingSenderId: "546570976928",
+            appId: "1:546570976928:web:807549c9383b99db8d53c5",
+            measurementId: "G-5WLY2QJMED"
+        };
